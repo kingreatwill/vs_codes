0bee8cb2ff8076e9fe8d613de6e7cd495f13c928
Administrator <Administrator@WZ-PC>
2017-05-04 17:30:49 +0800

    http modify

--- Lazywg/Lazywg.Common/Helper/HttpHelper.cs
+++ Lazywg/Lazywg.Common/Helper/HttpHelper.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -22,17 +24,7 @@ namespace Lazywg.Common.Helper
         /// <returns></returns>
         public static string HttpGet(string url, Dictionary<string, string> paramDict)
         {
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + "?" + GetStrParams(paramDict));
-                request.Method = "Get";
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                return GetResponseString(response);
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
+            return HttpGet(url, GetStrParams(paramDict));
         }
 
         /// <summary>
@@ -45,7 +37,7 @@ namespace Lazywg.Common.Helper
         {
             try
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + "?" + context);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + (string.IsNullOrEmpty(context) ? string.Empty : "?" + context));
                 request.Method = "Get";
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 return GetResponseString(response);
@@ -64,24 +56,7 @@ namespace Lazywg.Common.Helper
         /// <returns></returns>
         public static string HttpPost(string url, Dictionary<string, string> paramDict)
         {
-            try
-            {
-                byte[] byteArray = Encoding.UTF8.GetBytes(GetStrParams(paramDict)); //UTF8Encoding.GetBytes(GetStrParams(paramDict)); //转化
-                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(url));
-                webReq.Method = "POST";
-                webReq.ContentType = "application/x-www-form-urlencoded";
-
-                webReq.ContentLength = byteArray.Length;
-                Stream stream = webReq.GetRequestStream();
-                stream.Write(byteArray, 0, byteArray.Length);//写入参数
-                stream.Close();
-                HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
-                return GetResponseString(response);
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
+            return HttpPost(url, paramDict, Encoding.UTF8);
         }
 
         /// <summary>
@@ -91,26 +66,9 @@ namespace Lazywg.Common.Helper
         /// <param name="paramDict"></param>
         /// <param name="encoding"></param>
         /// <returns></returns>
-        public static string HttpPost(string url,Dictionary<string, string> paramDict,Encoding encoding)
+        public static string HttpPost(string url, Dictionary<string, string> paramDict, Encoding encoding)
         {
-            try
-            {
-                byte[] byteArray = encoding.GetBytes(paramDict.ToString()); 
-                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(url));
-                webReq.Method = "POST";
-                webReq.ContentType = "application/x-www-form-urlencoded";
-
-                webReq.ContentLength = byteArray.Length;
-                Stream stream = webReq.GetRequestStream();
-                stream.Write(byteArray, 0, byteArray.Length);//写入参数
-                stream.Close();
-                HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
-                return GetResponseString(response);
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
+            return HttpPost(url, GetStrParams(paramDict), encoding);
         }
 
         /// <summary>
@@ -127,7 +85,7 @@ namespace Lazywg.Common.Helper
                 byte[] byteArray = encoding.GetBytes(context);
                 HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(url));
                 webReq.Method = "POST";
-                webReq.ContentType = "application/x-www-form-urlencoded";
+                webReq.ContentType = "application/x-www-form-urlencoded";//表单默认数据提交格式
 
                 webReq.ContentLength = byteArray.Length;
                 Stream stream = webReq.GetRequestStream();
@@ -140,6 +98,60 @@ namespace Lazywg.Common.Helper
             {
                 throw e;
             }
+        }
+
+        public static string HttpClientGet(string url, Dictionary<string, string> paramDict)
+        {
+            return HttpClientGet(url, GetStrParams(paramDict));
+        }
+
+        public static string HttpClientGet(string url, string content)
+        {
+            if (url.StartsWith("https"))
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            HttpClient httpClient = new HttpClient();
+            url = url + (string.IsNullOrEmpty(content) ? string.Empty : "?" + content);
+            HttpResponseMessage response = httpClient.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+            return string.Empty;
+        }
+
+        public static string HttpClientPost(string url, string context)
+        {
+            return HttpClientPost(url, context, Encoding.UTF8);
+        }
+
+        public static string HttpClientPost(string url, Dictionary<string, string> paramDict)
+        {
+            return HttpClientPost(url, paramDict, Encoding.UTF8);
+        }
+
+        public static string HttpClientPost(string url, Dictionary<string, string> paramDict, Encoding encoding)
+        {
+            return HttpClientPost(url, GetStrParams(paramDict), encoding);
+        }
+
+        public static string HttpClientPost(string url, string context, Encoding encoding)
+        {
+            if (url.StartsWith("https"))
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            HttpContent httpContent = new StringContent(context);
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");//表单默认数据提交格式
+
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage response = httpClient.PostAsync(url, httpContent).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+            return string.Empty;
         }
 
         /// <summary>
@@ -166,7 +178,7 @@ namespace Lazywg.Common.Helper
             StringBuilder builder = new StringBuilder();
             foreach (var item in paramDict)
             {
-                builder.AppendFormat("{0}={1}&", item.Key,HttpUtility.UrlEncode(item.Value));
+                builder.AppendFormat("{0}={1}&", item.Key, HttpUtility.UrlEncode(item.Value));
             }
             string result = builder.ToString();
             return result.Substring(0, result.Length - 1);
